06343001f4051b2efb8d2b7403f4b50204ef0e69
dcloveUIUC <dclove2@illinois.edu>
2022-05-14 20:50:59 -0700

    now reads receiving phone number from YAML as well.

--- send_text.py
+++ send_text.py
@@ -1,29 +1,30 @@
 import yaml
+from twilio.rest import Client
 
-def send_text(text, phone_number):
+def send_text(text):
     '''
     Use pip3 install twilio to install the correct module.
-    send_text receives a string text message and a target phone number
-    that uses the twilio rest API to send a notification.
+    send_text receives a string text message and sends the text using the
+    Twilio API. Reads information from YAML file twilio_auth.yaml
     phone_number must be a string and include the country code (i.e. "+15551234567")
     '''
 
     with open(r"./twilio_auth.yaml") as file:
         auth_codes = yaml.load(file, Loader=yaml.FullLoader)
-
-    from twilio.rest import Client
     # Account SID :
     account_sid = auth_codes["account_sid"]
     # Auth Token :
     auth_token  = auth_codes["auth_token"]
     # Message-receiving phone number :
-    receiving_number = phone_number
+    receiving_number = auth_codes["receiving_number"]
     # Message-sending Twilio phone number :
     twilio_number = auth_codes["twilio_number"]
 
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to= phone_number,
+        to= receiving_number,
         from_= twilio_number,
         body= text)
+
+send_text("pajamas")
