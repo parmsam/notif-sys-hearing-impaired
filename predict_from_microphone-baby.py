b61c5cb742b74e2002efdeec090cfd06ff0f24ad
dcloveUIUC <dclove2@illinois.edu>
2022-05-14 21:15:06 -0700

    Added send_text function call into dog and baby prediction loops

--- predict_from_microphone-baby.py
+++ predict_from_microphone-baby.py
@@ -1,7 +1,9 @@
 import os
+import time
 
 from micmon.audio import AudioDevice
 from micmon.model import Model
+from send_text.py import send_text
 
 # Path to a previously saved sound detection Tensorflow model
 model_dir = os.path.expanduser('~/models/sound-detect-baby')
@@ -10,9 +12,16 @@ model = Model.load(model_dir)
 audio_system = 'alsa'        # Supported: alsa and pulse
 audio_device = 'plughw:1,0'  # Get list of recognized input devices with arecord -l
 
+last_positive_prediction = None
+
 with AudioDevice(audio_system, device=audio_device) as source:
     for sample in source:
         source.pause()  # Pause recording while we process the frame
         prediction = model.predict(sample)
+        #Checks for a positive prediction, waits 5 mintues between sending notifications
+        if prediction == "positive":
+            if (last_positive_prediction == None) or (time.time() - last_positive_prediction > 300):
+                last_positive_prediction = time.time()
+                send_text("Baby crying has been detected.")
         print(prediction)
         source.resume() # Resume recording
