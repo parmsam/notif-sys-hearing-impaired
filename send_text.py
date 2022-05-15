98f5b098add542ccdb3a639f807eb40d268108dd
dcloveUIUC <dclove2@illinois.edu>
2022-05-14 20:43:30 -0700

    Updated send_text to read from yaml file. Added gitignore to prevent auth codes from being exposed

--- send_text.py
+++ send_text.py
@@ -1,19 +1,25 @@
-def send_text(text, phone_number,auth_token):
+import yaml
+
+def send_text(text, phone_number):
     '''
     Use pip3 install twilio to install the correct module.
     send_text receives a string text message and a target phone number
     that uses the twilio rest API to send a notification.
     phone_number must be a string and include the country code (i.e. "+15551234567")
-    Requires Twilio auth_token.
     '''
+
+    with open(r"./twilio_auth.yaml") as file:
+        auth_codes = yaml.load(file, Loader=yaml.FullLoader)
+
     from twilio.rest import Client
     # Account SID :
-    account_sid = "AC8490704432ab402ed61a4d743b961f17"
+    account_sid = auth_codes["account_sid"]
+    # Auth Token :
+    auth_token  = auth_codes["auth_token"]
     # Message-receiving phone number :
     receiving_number = phone_number
     # Message-sending Twilio phone number :
-    twilio_number = "+19379303467"
-
+    twilio_number = auth_codes["twilio_number"]
 
     client = Client(account_sid, auth_token)
 
@@ -21,5 +27,3 @@ def send_text(text, phone_number,auth_token):
         to= phone_number,
         from_= twilio_number,
         body= text)
-
-    print(message.sid)
